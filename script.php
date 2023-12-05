7742b383aedffcc4a1dcfc0cc0e2999dee8e255d
unknown <it@thinksurfmedia.com>
2023-12-05 13:08:58 +0530

    3 commit

--- script.php
+++ script.php
@@ -3,5 +3,5 @@
 //Password:derdWKqa0782Z
 //https://github.com/cinecafes/cinecafes.git
 
-exec("git pull https://github.com/tsmgit/1callatmservice.git main");
+exec("git pull https://github.com/1callatmservice/1callatm.git main");
 ?>
