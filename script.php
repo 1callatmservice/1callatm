c22024858bdd0cdd94ea3bbcba4b3c1a36bcf54e
unknown <it@thinksurfmedia.com>
2023-12-05 13:23:03 +0530

    8 commit

--- script.php
+++ script.php
@@ -4,4 +4,6 @@
 //https://github.com/cinecafes/cinecafes.git
 
 exec("git pull https://github.com/1callatmservice/1callatm.git main");
+exec("cd /home6/thinksurfmedia/1callatmservice1.thinksurfmedia.in/");
+exec("composer install");
 ?>
